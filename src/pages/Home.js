4afe9eeba2c89f8a021a438c626dcfda8e734c92
eswarsai959@gmail.com <eswar@hbits.co>
2020-03-08 01:03:51 +0530

    services component and title component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
+
 import {Link} from 'react-router-dom';
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at Rs.599">
-        <Link to='/rooms' className="btn-primary">Our Rooms</Link>
-      </Banner>
-    </Hero>
+    <Fragment>
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at Rs.599">
+          <Link to='/rooms' className="btn-primary">Our Rooms</Link>
+        </Banner>
+      </Hero>
+      <Services/>
+    </Fragment>
   )
 }
