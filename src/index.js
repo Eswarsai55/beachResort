2fa19499919c6e4c716f759f89ae57be55414fae
eswarsai959@gmail.com <eswar@hbits.co>
2020-03-08 18:04:49 +0530

    Creting provider and consumer for context and using the class component data

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router, } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import {RoomProvider} from './Context';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root')
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>, document.getElementById('root')
+  
 );
 
 // If you want your app to work offline and load faster, you can change
